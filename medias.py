7a1c2eb2fcde461e9fc0996f9cb9047878c40999
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2025-02-06 12:30:41 -0600

    Plotly express a Graph Objects para medias

--- medias.py
+++ medias.py
@@ -2,6 +2,7 @@ import pyarrow
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 NIVELES_GRADO = {
     "Preescolar": [3],
@@ -10,6 +11,13 @@ NIVELES_GRADO = {
     }
 
 COLORES = ["#8338ec", "#ffca3a", "#8ac926", "#1982c4"]
+COLORES_PROCESO = {
+    "Recuperación de información":"#ff595e",
+    "Comprensión":"#ffca3a",
+    "Análisis":"#8ac926",
+    "Utilización del conocimiento":"#1982c4",
+    "No definido":"#ffffff",
+}
 
 diccionario = pd.read_parquet("data/diccionario.parquet")
 diccionario = diccionario.drop(["grado"], axis=1)
@@ -49,10 +57,43 @@ medias_filtro = medias_filtro.loc[
     (medias_filtro["proceso"].isin(sel_cnt_proceso))
 ]
 
-orden = st.radio("Ordenar por:", ["Proceso", "Media en la rúbrica"])
-if orden == "Media en la rúbrica":
-#if por_media:
-    medias_filtro = medias_filtro.sort_values(["media", "proceso", "consigna", "inciso", "criterio_num"], ascending=False)
+orden = st.radio(
+    "Ordenar por:",
+    ["Proceso", "Media", "Reactivo"],
+    horizontal=True
+    )
+
+for eia in medias_filtro["eia"].unique():
+    st.markdown(f"### {eia}")
+    medias_filtro_eia = medias_filtro.loc[medias_filtro["eia"] == eia]
+
+    if orden == "Proceso":
+        medias_filtro_eia = medias_filtro_eia.sort_values("proceso")
+    elif orden == "Reactivo":
+        medias_filtro_eia = medias_filtro_eia.sort_values("item")
+    elif orden == "Media":
+        medias_filtro_eia = medias_filtro_eia.sort_values("media", ascending=False)
+
+    figura = go.Figure(go.Scatter(
+        x=medias_filtro_eia["item"],
+        y=medias_filtro_eia["media"],
+        mode="lines",
+        line=dict(color="#ffffff", width=1),
+        showlegend=False,
+        ))
+    for proceso in medias_filtro_eia["proceso"].unique():
+        medias_proceso = medias_filtro_eia.loc[medias_filtro_eia["proceso"] == proceso]
+        figura.add_trace(go.Scatter(
+            x=medias_proceso["item"],
+            y=medias_proceso["media"],
+            mode="markers",
+            name=proceso,
+            marker=dict(color=COLORES_PROCESO[proceso], size=10),
+            hovertext=medias_proceso["campo"],
+            ))
+    st.plotly_chart(figura)
+    st.dataframe(medias_filtro_eia[["item", "proceso", "media", "campo", "pda", "descriptor", "criterio"]])
+
 
 for eia in medias_filtro["eia"].unique():
     st.markdown(f"### {eia}")
