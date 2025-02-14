bde6c685eb82347478a992d572a00cd931e57e4d
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2025-02-14 13:46:42 -0600

    Colores y filtros

--- perfiles.py
+++ perfiles.py
@@ -11,13 +11,13 @@ NIVELES_GRADO = {
     }
 COLORES_RESP = dict(zip(
     ["N0", "N1", "N2", "N3"], 
-    ["#fcb1c3", "#fce397", "#bae673", "#a4dafc"]
+    ["#fcb1c3", "#ef476f", "#f5b700", "#008bf8"]
     ))
 
 # Diccionario de variables
 diccionario = pd.read_parquet("data/diccionario.parquet")
 diccionario = diccionario.drop([
-    "fase", "grado", "eia_clave", "campo_clave", "pda_grado", 
+    "fase", "grado", "eia_clave", "pda_grado", 
     "criterio_clave", "peso_max", "ponderador",
     ], axis=1)
 rubrica     = pd.read_parquet("data/diccionario_rubrica.parquet")
@@ -83,39 +83,50 @@ for eia in irt_filtro["eia"].unique():
         ["dificultad"]
         .values.round(2)
     )
+    
     sel_dif = st.select_slider(
         "Corte",
         options=dificultades,
         key=f"slider_{eia}"
         )
+    sel_texto = st.radio(
+        "Texto para mostrar.", 
+        options=["Dificultad", "Proceso", "Campo formativo"],
+        horizontal=True,
+        key=f"texto_{eia}",
+        )
+    if sel_texto == "Campo formativo":
+        sel_texto = "campo_clave"
+    else:
+        sel_texto = sel_texto.lower()
     
-    fig = go.Figure(go.Scatter(
-        x = irt_eia["item"],
-        y = irt_eia["dificultad"],
-        mode="markers",
-    ))
+    fig = go.Figure()
     fig.add_hline(
         y=sel_dif,
         line_width=1,
         line_color="#858ae3"
         )
-    fig.update_xaxes(title_text="Criterios")
-    fig.update_yaxes(title_text="Dificultad")
-    fig.update_layout(
-        height=400,
-        margin=dict(t=30, b=15),
-    )
+    for resp in irt_eia["resp"].unique():
+        irt_resp = irt_eia.loc[irt_eia["resp"] == resp]
+        fig.add_trace(go.Scatter(
+            x=irt_resp["item"],
+            y=irt_resp["dificultad"],
+            name=resp,
+            mode="markers+text",
+            text= round(irt_resp[sel_texto], 2),
+            textposition="top center",
+            hovertext=irt_resp["criterio"],
+            marker=dict(color=COLORES_RESP[resp])
+        ))
+        fig.update_xaxes(title_text="Criterios")
+        fig.update_yaxes(title_text="Dificultad")
+        fig.update_layout(
+            height=400,
+            margin=dict(t=30, b=15),
+        )
     
     personas_cuantil = personas_eia.loc[personas_eia["puntaje"] <= float(sel_dif), ["cuantil", "puntaje"]].reset_index(drop=True)
     personas_cuantil["puntaje"] = personas_cuantil["puntaje"].round(3)
-    #irt_cuantil = (
-    #    irt_eia.loc[
-    #        irt_eia["dificultad"].round(2) >= sel_dif, 
-    #        ["item", "resp", "dificultad", "proceso", "criterio", "descriptor", "campo"]
-    #        ]
-    #        .sort_values("dificultad")
-    #        .reset_index(drop=True)
-    #    )
     irt_cuantil = irt_eia.sort_values("dificultad")
     irt_cuantil["posicion"] = ["Arriba" if i >= sel_dif else "Abajo" for i in irt_cuantil["dificultad"]]
     irt_cuantil = irt_cuantil[["item", "resp", "dificultad", "posicion", "proceso", "criterio", "descriptor", "campo"]]
@@ -125,5 +136,5 @@ for eia in irt_filtro["eia"].unique():
     st.markdown("### Población debajo del punto de corte.")
     st.dataframe(personas_cuantil.iloc[-1])
     st.markdown("### Posición de los criterios respecto al punto de corte.")
-    posiciones = st.multiselect("Mostrar sólo criterios que están:", options=["Arriba", "Abajo"], default=["Arriba", "Abajo"])
+    posiciones = st.multiselect("Mostrar sólo criterios que están:", options=["Arriba", "Abajo"], default=["Arriba", "Abajo"], key=f"posicion_{eia}")
     st.dataframe(irt_cuantil.loc[irt_cuantil["posicion"].isin(posiciones)])
