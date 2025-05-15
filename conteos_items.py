0add0dfee5ac3220f274267f84bb4ce7e24b11ca
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2025-05-15 17:15:32 -0600

    Conteo por criterio a polars

--- conteos_items.py
+++ conteos_items.py
@@ -1,6 +1,6 @@
 import pyarrow
 import streamlit as st
-import pandas as pd
+import polars as pl
 import plotly.graph_objects as go
 
 NIVELES_GRADO = {
@@ -14,78 +14,56 @@ COLORES_RESP = dict(zip(["N0", "N1", "N2", "N3"], COLORES))
 
 COLS_INFORMACION = ["campo", "contenido", "pda", "descriptor", "proceso"]
 
-diccionario = pd.read_parquet("data/diccionario.parquet")
-diccionario = diccionario.drop(["fase", "nivel", "grado"], axis=1)
-rubrica = pd.read_parquet("data/diccionario_rubrica.parquet")
-
-conteo_grado = pd.read_parquet("data/item_conteo_grado.parquet")
-conteo_grado = conteo_grado.merge(diccionario, how="inner", on="item").merge(
-    rubrica, how="inner", on=["item", "resp"]
-)
-
-conteo_grado["resp"] = (
-    conteo_grado["resp"]
-    .astype("category")
-    .cat.reorder_categories(["N0", "N1", "N2", "N3"], ordered=True)
-)
-
-conteo_grado["consigna"] = conteo_grado["consigna"].astype("int").astype("str")
-conteo_grado["grado"] = conteo_grado["grado"].astype("int").astype("str")
-
-
-nivel_0 = conteo_grado.loc[conteo_grado["resp"] == "N0"][
-    ["item", "grado", "prop"]
-].rename(columns={"prop": "nivel_0"})
-nivel_3 = conteo_grado.loc[conteo_grado["resp"] == "N3"][
-    ["item", "grado", "prop"]
-].rename(columns={"prop": "nivel_3"})
-conteo_grado = conteo_grado.merge(nivel_0, on=["item", "grado"], how="left").merge(
-    nivel_3, on=["item", "grado"], how="left"
-)
-
-#### Streamlit ####
-
 st.set_page_config(
     page_title="Conteos por criterio - Evaluación diagnóstica 2024",
     page_icon=":worm:",
     layout="wide",
 )
 
-with st.sidebar:
-    niveles = conteo_grado["nivel"].unique()
-    sel_nivel = st.selectbox("Nivel", options=niveles, index=2)
-    conteo_filtro = conteo_grado.loc[conteo_grado["nivel"] == sel_nivel]
+diccionario = pl.read_parquet("data/diccionario.parquet").drop(["fase", "nivel", "grado"])
+rubrica = pl.read_parquet("data/diccionario_rubrica.parquet")
+conteo = pl.read_parquet("data/item_conteo_grado.parquet")
+conteo = conteo.join(diccionario, how="inner", on="item").join(rubrica, how="inner", on=["item", "resp"])
 
-    eias = conteo_filtro["eia"].unique()
-    sel_eia = st.selectbox("EIA", options=eias)
-    conteo_filtro = conteo_filtro.loc[conteo_filtro["eia"] == sel_eia]
+conteo = conteo.with_columns(
+    pl.col(["consigna", "grado"]).cast(pl.Int16).cast(pl.String)
+)
 
-    consignas = conteo_filtro["consigna"].unique()
-    consignas.sort()
-    sel_consigna = st.selectbox("Consigna", options=consignas)
-    conteo_filtro = conteo_filtro.loc[conteo_filtro["consigna"] == sel_consigna]
+nivel_0 = conteo.filter(pl.col("resp") == "N0").select(
+    ["item", "grado", "prop"]).rename({"prop": "nivel_0"})
+nivel_3 = conteo.filter(pl.col("resp") == "N3").select(
+    ["item", "grado", "prop"]).rename({"prop": "nivel_3"})
+conteo = conteo.join(nivel_0, on=["item", "grado"], how="left").join(
+    nivel_3, on=["item", "grado"], how="left"
+)
+
+#### Streamlit ####
+eias = conteo["eia"].unique(maintain_order=True)
+sel_eia = st.selectbox("EIA", options=eias)
+conteo_filtro = conteo.filter(pl.col("eia") == sel_eia)
+consignas = conteo_filtro["consigna"].unique().sort()
+sel_consigna = st.selectbox("Consigna", options=consignas)
+conteo_filtro = conteo_filtro.filter(pl.col("consigna") == sel_consigna)
 
 st.markdown(f"# {conteo_filtro['eia'].unique()[0]} (Consigna {sel_consigna})")
 
 criterios = conteo_filtro["criterio"].unique()
 sel_criterios = st.multiselect("Criterios", options=criterios, default=criterios)
-conteo_filtro = conteo_filtro.loc[conteo_filtro["criterio"].isin(sel_criterios)]
-
+conteo_filtro = conteo_filtro.filter(pl.col("criterio").is_in(sel_criterios))
 
 for criterio in sel_criterios:
     st.divider()
     st.markdown(f"## {criterio}")
-    conteo_criterio = conteo_filtro.loc[conteo_filtro["criterio"] == criterio]
-
+    conteo_criterio = conteo_filtro.filter(pl.col("criterio") == criterio)
     figura = go.Figure()
-    for resp in conteo_criterio["resp"].unique():
-        conteo_resp = conteo_criterio.loc[conteo_criterio["resp"] == resp]
+    for resp in conteo_criterio["resp"].unique(maintain_order=True):
+        conteo_resp = conteo_criterio.filter(pl.col("resp") == resp)
         figura.add_trace(
             go.Bar(
                 y=conteo_resp["grado"],
                 x=conteo_resp["prop"],
                 name=resp,
-                text=round(conteo_resp["prop"]),
+                text=conteo_resp["prop"].round(1),
                 insidetextanchor="middle",
                 marker=dict(color=COLORES_RESP[resp]),
                 orientation="h",
@@ -100,7 +78,7 @@ for criterio in sel_criterios:
             title="Grado",
             tickmode="array",
             tickvals=conteo_criterio["grado"],
-            ticktext=conteo_criterio["grado"].unique(),
+            ticktext=conteo_criterio["grado"].unique(maintain_order=True),
             autorange="reversed",
         ),
         xaxis=dict(title="Porcentaje"),
@@ -112,22 +90,22 @@ for criterio in sel_criterios:
     st.table(
         (
             conteo_criterio[COLS_INFORMACION]
-            .rename(str.title, axis="columns")
-            .drop_duplicates()
-            .reset_index(drop=True)
+            .rename(str.title)
+            .unique(maintain_order=True).to_pandas().set_index("Campo")
         )
     )
     conteo_criterio = (
         conteo_criterio[["resp", "resp_nivel", "resp_rubrica"]]
-        .drop_duplicates()
+        .unique(maintain_order=True)
         .rename(
-            columns={
+            {
                 "resp": "Respuesta",
                 "resp_nivel": "Nivel",
                 "resp_rubrica": "Rúbrica",
             }
         )
-        .reset_index(drop=True)
+        .to_pandas()
+        .set_index("Respuesta")
     )
     st.markdown("### Niveles de la rúbrica")
     st.table(conteo_criterio)
