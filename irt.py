7c41fdf00ca2f990fef0724267ec66e720527a21
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2025-05-16 16:08:38 -0600

    item IRT con polars

--- irt.py
+++ irt.py
@@ -1,7 +1,6 @@
 import pyarrow
 import streamlit as st
-import pandas as pd
-import plotly.express as px
+import polars as pl
 import plotly.graph_objects as go
 
 NIVELES_GRADO = {
@@ -12,22 +11,13 @@ NIVELES_GRADO = {
 
 COLORES = ["#8338ec", "#ffca3a", "#8ac926", "#1982c4", "#cccccc"]
 PROCESOS = [
-    "Recuperación de información",
     "Comprensión",
-    "Análisis",
     "Utilización del conocimiento",
+    "Propuesta de solución",
+    "Juicio crítico",
     "No definido",
 ]
 COLORES_PROCESO = dict(zip(PROCESOS, COLORES))
-
-diccionario = pd.read_parquet("data/diccionario.parquet")
-diccionario = diccionario.drop(["grado"], axis=1)
-
-irt = pd.read_parquet("data/item_irt.parquet")
-
-irt = irt.sort_values("dificultad")
-procesos = irt["proceso"].unique()
-campos = irt["campo"].unique()
 
 st.set_page_config(
     page_title="IRT - Evaluación diagnóstica 2024",
@@ -35,30 +25,38 @@ st.set_page_config(
     layout="wide",
 )
 
-#### Streamlit ####
+diccionario = pl.read_parquet("data/diccionario.parquet").drop(["grado"])
 
+irt = pl.read_parquet("data/item_irt.parquet")
+irt = irt.sort("dificultad")
+procesos = irt["proceso"].unique(maintain_order=True)
+campos = irt["campo"].unique(maintain_order=True)
+
+#### Streamlit ####
 st.title("IRT Evaluación Diagnóstica 2024")
 
 with st.sidebar:
     sel_nivel = st.selectbox("Nivel", options=NIVELES_GRADO.keys(), index=2)
     sel_grado = st.selectbox("Grado", options=NIVELES_GRADO[sel_nivel])
 
-irt_filtro = irt.loc[
-    (irt["nivel"] == sel_nivel) & (irt["grado"] == sel_grado)
-].sort_values(["proceso", "consigna", "inciso", "criterio_num"])
+irt_filtro = irt.filter(
+    pl.col("nivel") == sel_nivel,
+    pl.col("grado") == sel_grado,
+)
+#.sort(["proceso", "consigna", "inciso", "criterio_num"])
 
-eia_filtro = irt_filtro["eia"].unique()
+eia_filtro = irt_filtro["eia"].unique(maintain_order=True)
 
 with st.sidebar:
     sel_eia = st.multiselect("EIA", options=eia_filtro, default=eia_filtro)
     sel_proceso = st.multiselect("Proceso", options=procesos, default=procesos)
     sel_campo = st.multiselect("Campo formativo", options=campos, default=campos)
 
-medias_filtro = irt_filtro.loc[
-    (irt_filtro["eia"].isin(sel_eia))
-    & (irt_filtro["proceso"].isin(sel_proceso))
-    & (irt_filtro["campo"].isin(sel_campo))
-]
+medias_filtro = irt_filtro.filter(
+    pl.col("eia").is_in(sel_eia),
+    pl.col("proceso").is_in(sel_proceso),
+    pl.col("campo").is_in(sel_campo),
+)
 
 orden = st.radio("Ordenar por:", ["Proceso", "Dificultad", "Reactivo"], horizontal=True)
 if st.checkbox("Limites 0-800"):
@@ -68,14 +66,14 @@ else:
 
 for eia in medias_filtro["eia"].unique():
     st.markdown(f"### {eia}")
-    irt_filtro_eia = medias_filtro.loc[medias_filtro["eia"] == eia]
+    irt_filtro_eia = medias_filtro.filter(pl.col("eia") == eia)
 
     if orden == "Proceso":
-        irt_filtro_eia = irt_filtro_eia.sort_values(["proceso", "dificultad"])
+        irt_filtro_eia = irt_filtro_eia.sort(["proceso", "dificultad"])
     elif orden == "Dificultad":
-        irt_filtro_eia = irt_filtro_eia.sort_values("dificultad")
+        irt_filtro_eia = irt_filtro_eia.sort("dificultad")
     elif orden == "Reactivo":
-        irt_filtro_eia = irt_filtro_eia.sort_values(
+        irt_filtro_eia = irt_filtro_eia.sort(
             ["consigna", "inciso", "item_clave"]
         )
 
@@ -88,8 +86,8 @@ for eia in medias_filtro["eia"].unique():
             showlegend=False,
         )
     )
-    for proceso in irt_filtro_eia["proceso"].unique():
-        medias_proceso = irt_filtro_eia.loc[irt_filtro_eia["proceso"] == proceso]
+    for proceso in irt_filtro_eia["proceso"].unique(maintain_order=True):
+        medias_proceso = irt_filtro_eia.filter(pl.col("proceso") == proceso)
         figura.add_trace(
             go.Scatter(
                 x=medias_proceso["item_clave"],
@@ -102,7 +100,7 @@ for eia in medias_filtro["eia"].unique():
         )
     figura.update_yaxes(range=limites_y)
     st.plotly_chart(figura)
-    campos_eia = irt_filtro_eia["campo"].unique()
+    campos_eia = irt_filtro_eia["campo"].unique(maintain_order=True)
     st.dataframe(
         irt_filtro_eia[
             [
